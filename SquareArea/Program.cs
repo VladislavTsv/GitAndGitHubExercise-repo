8f92284dcf9b1b2143d3af61e04d0fa9cc08fde2
VladislavTsv <vl.tsvetanov@abv.bg>
2017-01-23 16:29:09 -0800

    Added some files

---
+++ SquareArea/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            double a = double.Parse(Console.ReadLine());
+            var area = a * a;
+            Console.WriteLine("Area = {0}", area);
+        }
+    }
+}
