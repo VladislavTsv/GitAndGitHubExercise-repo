f34c9a26d650fa85052ffa8d30105ac357ac957e
VladislavTsv <vl.tsvetanov@abv.bg>
2017-01-25 12:43:04 -0800

    Added some changes in Solution1

---
+++ RectangleArea/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Add the data of the sides:");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            var rectangleArea = a * b;
+            Console.WriteLine("The area = {0}", rectangleArea);
+        }
+    }
+}
